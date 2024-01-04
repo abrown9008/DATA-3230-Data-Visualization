7443d46752f256ab1ba986c57ddcd255c75416e0
Austin R. Brown, Ph.D <54994547+abrown9008@users.noreply.github.com>
2024-01-04 11:25:31 -0500

    Add files via upload

---
+++ Introduction to R and RStudio/DATA 3230 Intro to R Code.R
@@ -0,0 +1,106 @@
+## DATA 3230 - Introduction to R Code ##
+
+## R as a Calculator ##
+
+## Add ##
+
+2 + 2
+
+## Subtract ##
+
+2 - 2
+
+## Multiply ##
+
+2*2 
+
+## Divide ##
+
+2/2 
+
+## Raise to a Power ##
+
+2^2
+
+## or ##
+
+2**2
+
+## I can save 2 + 2 as "a" ##
+
+a <- 2 + 2
+
+a
+
+## R is case sensitive so big A isn't
+## the same as little a ##
+
+A
+
+b <- "DATA 3230"
+
+b
+
+## Importing a CSV File ##
+
+## First, install the readr package ##
+
+install.packages('readr')
+
+## What are the arguments for read_csv? ##
+
+?readr::read_csv
+
+## Go Ahead and Read it in ##
+
+heart <- readr::read_csv("HEART.csv")
+
+## Importing an XLSX File ##
+
+## Installing the 'readxl' package ##
+
+install.packages('readxl')
+
+## Importing the 'esoph' dataset ##
+
+esoph <- readxl::read_xlsx("esoph.xlsx")
+
+## We can explore some characteristics of our datasets
+## using the dplyr::glimpse function! ##
+
+## We have to first install the dplyr package ##
+
+install.packages('dplyr')
+
+heart |>
+  dplyr::glimpse()
+
+## Find the mean of AgeAtDeath ##
+
+mean(heart$AgeAtDeath)
+
+## Check the documentation for the mean function ##
+
+?mean
+
+## Try Again Omitting the NA Values ##
+
+mean(heart$AgeAtDeath,na.rm=TRUE)
+
+## Subset last four column of Heart ##
+
+heart_status <- heart |>
+  dplyr::select(Chol_Status,BP_Status,
+                Weight_Status,Smoking_Status) 
+
+## Subset Overweight Participants ##
+
+heart_status_ow <- heart_status |>
+  dplyr::filter(Weight_Status == 'Overweight')
+
+## Checking to Make Sure Subset Worked ##
+
+heart_status |>
+  dplyr::select(Weight_Status) |>
+  dplyr::group_by(Weight_Status) |>
+  dplyr::count()
