d4b543adcc865fab4336648395f884ef8cb5ac58
abrown9008 <abrow708@kennesaw.edu>
2024-07-30 12:24:57 -0400

    Updating variation code to match book

--- Communicating Variation/DATA 3230 Communicating Variation Code.R
+++ Communicating Variation/DATA 3230 Communicating Variation Code.R
@@ -9,42 +9,25 @@ library(tidyverse)
 ## who have thrown more than 15 innings: ##
 
 so9 <- Lahman::Pitching |>
-  filter(between(yearID,1910,2023) & (IPouts/3) >= 15)
-
-## Then, we need to create a new variable called "SO9" 
-## to calculate the number of strikeouts a given pitcher 
-## had during a given season. 
-## Note, the variable IPouts is the number of innings 
-## pitched times 3: ##
-
-so9 <- Lahman::Pitching |>
-  filter(between(yearID,1910,2023) & (IPouts/3) >= 15) |>
-  mutate(SO9 = SO/(IPouts/3)*9)
-
-## Now we have to decide how we're going to visualize these
-## data. A straightforward way may be by plotting the mean
-## values for each year and then connecting them with a line
-## in what's called a 'time series plot' ##
-
-## First, we have to get the mean SO9 for each year: ##
-
-so9 <- Lahman::Pitching |>
   filter(between(yearID,1910,2023) & (IPouts/3) >= 15) |>
   mutate(SO9 = SO/(IPouts/3)*9) |>
   group_by(yearID) |>
-  summarize(SO9 = mean(SO9,na.rm=T))
+  summarize(Mean_SO9 = mean(SO9,na.rm=T))
+
+so9 |>
+  glimpse()
 
 ## Now we can make the visualization. But instead of 
 ## geom_bar, we are going to use geom_point and geom_line: ##
 
 so9 |>
-  ggplot(aes(x=yearID,y=SO9)) +
+  ggplot(aes(x=yearID,y=Mean_SO9)) +
   geom_point() +
   geom_line() +
   labs(x="Season",
        y="Average Number of Strikeouts per 9 Innings Pitched",
        title="Changes in Strikeouts per 9 Innings Pitched",
-       subtitle="Between the 1910 & 2023 Regular Seasons") +
+       subtitle="Between the 1910 & 2020 Regular Seasons") +
   theme_classic()
 
 ## This plot is well and good, but what if we wanted to 
@@ -58,15 +41,18 @@ so9 |>
 ## SO9 in its own dataframe to make the annotations: ##
 
 minmax <- so9 |>
-  filter(SO9 == min(SO9) | SO9 == max(SO9))
+  filter(Mean_SO9 == min(Mean_SO9) | Mean_SO9 == max(Mean_SO9))
+
+minmax |>
+  glimpse()
 
 ## So now we can plot! ##
 
 so9 |>
-  ggplot(aes(x=yearID,y=SO9)) +
+  ggplot(aes(x=yearID,y=Mean_SO9)) +
   geom_point() +
   geom_line() +
-  geom_text(aes(label=SO9),data=minmax) +
+  geom_text(aes(label=Mean_SO9),data=minmax) +
   labs(x="Season",
        y="Average Number of Strikeouts per 9 Innings Pitched",
        title="Changes in Strikeouts per 9 Innings Pitched",
@@ -80,16 +66,17 @@ so9 |>
 ## problems with relative ease: ##
 
 so9 |>
-  ggplot(aes(x=yearID,y=SO9)) +
+  ggplot(aes(x=yearID,y=Mean_SO9)) +
   geom_point() +
   geom_line() +
-  geom_text(aes(label=round(SO9,2)),
+  geom_text(aes(label=round(Mean_SO9,2)),
             vjust = -1,data=minmax) +
   labs(x="Season",
        y="Average Number of Strikeouts per 9 Innings Pitched",
        title="Changes in Strikeouts per 9 Innings Pitched",
        subtitle="Between the 1910 & 2023 Regular Seasons") +
-  theme_classic()
+  theme_classic() +
+  scale_y_continuous(limits=c(2,11))
 
 ## Okay, so that problem is resolved. But what's another problem?
 ## The main one is: What in the world do these values represent and
@@ -97,44 +84,30 @@ so9 |>
 ## We can write out a sentence we want rendered for both the minimum
 ## and maximum values: ##
 
-minmax2 <- minmax |>
-  mutate(Text = paste(round(SO9,2),", which was observed in ",
-  yearID, ",\n was the",if_else(SO9 == min(SO9)," minimum", " maximum"), 
-  " SO/9",
-  sep = ""))
+minmax <- minmax |>
+  mutate(label = case_when(Mean_SO9 == min(Mean_SO9) ~ paste(yearID,"Lowest SO/9\nin 1910-2023"),
+                           Mean_SO9 == max(Mean_SO9) ~ paste(yearID,"Highest SO/9\nin 1910-2023"))
+  )
 
-minmax2
+minmax |>
+  glimpse()
 
 ## Cool, so now we can plot this text on the graph! ##
 
 so9 |>
-  ggplot(aes(x=yearID,y=SO9)) +
+  ggplot(aes(x=yearID,y=Mean_SO9)) +
   geom_point() +
   geom_line() +
-  geom_text(aes(label=Text),
-            vjust = -1,data=minmax2) +
+  geom_text(aes(label=label),
+            vjust = -1,data=minmax) +
   labs(x="Season",
        y="Average Number of Strikeouts per 9 Innings Pitched",
        title="Changes in Strikeouts per 9 Innings Pitched",
        subtitle="Between the 1910 & 2023 Regular Seasons") +
-  theme_classic()
-
-## Well now we have big problems with how the text is rendering!! ##
-## Let's change the y-axis length and see if that helps ##
-
-so9 |>
-  ggplot(aes(x=yearID,y=SO9)) +
-  geom_point() +
-  geom_line() +
-  geom_text(aes(label=Text),
-            vjust = -1,data=minmax2) +
-  labs(x="Season",
-       y="Average Number of Strikeouts per 9 Innings Pitched",
-       title="Changes in Strikeouts per 9 Innings Pitched",
-       subtitle="Between the 1910 & 2023 Regular Seasons") +
-  theme_classic() +
-  scale_y_continuous(limits = c(1,12),
-                     breaks = seq(1,13))
+  theme_classic()  +
+  scale_y_continuous(limits=c(2,12),
+                     breaks = seq(1,13)) +
+  scale_x_continuous(limits = c(1910,2027))
 
 ## Better but still problems! Here, let's try to use a new 
 ## function within a package called ggrepel ##
@@ -142,17 +115,25 @@ so9 |>
 library(ggrepel)
 
 so9 |>
-  ggplot(aes(x=yearID,y=SO9)) +
+  ggplot(aes(x=yearID,y=Mean_SO9)) +
   geom_point() +
   geom_line() +
-  geom_label_repel(aes(label=Text),data=minmax2) +
+  geom_label_repel(aes(label=label),
+                   family='serif',
+                   color='turquoise',
+                   fontface='bold',
+                   size=3,
+                   nudge_y = 1,
+                   alpha = 0.8,
+                   data=minmax) +
   labs(x="Season",
        y="Average Number of Strikeouts per 9 Innings Pitched",
        title="Changes in Strikeouts per 9 Innings Pitched",
        subtitle="Between the 1910 & 2023 Regular Seasons") +
-  theme_classic() +
-  scale_y_continuous(limits = c(1,12),
-                     breaks = seq(1,13))
+  theme_classic()  +
+  scale_y_continuous(limits=c(2,12),
+                     breaks = seq(1,13)) +
+  scale_x_continuous(limits = c(1910,2027))
 
 ## We can play around with the font in geom_label_repel
 ## like we did previously: ##
@@ -176,166 +157,106 @@ so9 |>
   scale_y_continuous(limits = c(1,12),
                      breaks = seq(1,13))
 
-## Baseball was famously segregated prior to the 1947
-## season when Jackie Robinson was the first Black player
-## for the Brooklyn Dodgers. If we look on the graph,
-## we can see that right about at the end of segregation,
-## SO/9 started to go up. The segregated and integrated
-## eras of MLB are worth highlighting. We can do this 
-## with a couple of cool tools. First, is geom_area: ##
+## Time Series Plot with Standard Errors ##
+
+## We can also visualize the within-year variation by
+## adding standard error bars to our points! ##
+
+## To do this we will reaggregate our data using rstatix ##
+
+library(rstatix)
+
+so9 <- Lahman::Pitching |>
+  filter(between(yearID,1910,2023) & (IPouts/3) >= 15) |>
+  mutate(SO9 = SO/(IPouts/3)*9) |>
+  group_by(yearID) |>
+  get_summary_stats(SO9,type='full') |>
+  select(yearID,mean,se)
 
 so9 |>
-  ggplot(aes(x=yearID,y=SO9)) +
+  glimpse()
+
+## Now we can plot using geom_errorbar ##
+
+so9 |>
+  ggplot(aes(x=yearID,y=mean)) +
   geom_point() +
+  geom_errorbar(aes(ymin=mean-se,ymax=mean+se)) +
   geom_line() +
-  geom_area(aes(y=SO9)) +
-  geom_label_repel(aes(label=Text),
-                   family = 'serif',
-                   color = 'turquoise',
-                   fontface = 'bold',
-                   nudge_y = 1.5,
-                   size = 3,
-                   data=minmax2)+
+  labs(x="Season",
+       y="Average Number of Strikeouts per 9 Innings Pitched",
+       title="Changes in Strikeouts per 9 Innings Pitched",
+       subtitle="Between the 1910 & 2023 Regular Seasons") +
+  theme_classic()
+
+## Boxplots ##
+
+## For as wide of an x-axis as we have, it is hard to get 
+## a sense of within year variation using the above method.
+## Instead, it may be easier to achieve this goal using a boxplot ##
+
+## However, to visualize a boxplot, we need the data to be in
+## its raw form: ##
+
+so9 <- Lahman::Pitching |>
+  filter(between(yearID,1910,2023) & (IPouts/3) >= 15) |>
+  mutate(SO9 = SO/(IPouts/3)*9)
+
+so9 |>
+  glimpse()
+
+## Now, we can use geom_boxplot to visualize ##
+
+so9 |>
+  ggplot(aes(x=factor(yearID),y=SO9)) +
+  geom_boxplot(fill='white',color='black') +
+  labs(x="Season",
+       y="Average Number of Strikeouts per 9 Innings Pitched",
+       title="Changes in Strikeouts per 9 Innings Pitched",
+       subtitle="Between the 1910 & 2023 Regular Seasons") +
+  theme_classic()
+
+## Formatting Tick Marks ##
+
+## Changing tick label angle ##
+
+so9 |>
+  ggplot(aes(x=factor(yearID),y=SO9)) +
+  geom_boxplot(fill='white',color='black') +
   labs(x="Season",
        y="Average Number of Strikeouts per 9 Innings Pitched",
        title="Changes in Strikeouts per 9 Innings Pitched",
        subtitle="Between the 1910 & 2023 Regular Seasons") +
   theme_classic() +
-  scale_y_continuous(limits = c(0,12),
-                     breaks = seq(1,13))
+  theme(axis.text.x = element_text(angle=45))
 
-## Notice I had to change the minimum of the yaxis to 0 ##
-
-## Okay the color is completely opaque!! We can change this
-## with alpha where 0 is totally transparent and 1 is
-## totally opaque. Let's try 0.25 ##
+## Changing number of tick labels ##
 
 so9 |>
-  ggplot(aes(x=yearID,y=SO9)) +
-  geom_point() +
-  geom_line() +
-  geom_area(aes(y=SO9),
-            fill='blue',
-            alpha = 0.25) +
-  geom_label_repel(aes(label=Text),
-                   family = 'serif',
-                   color = 'turquoise',
-                   fontface = 'bold',
-                   nudge_y = 1.5,
-                   size = 3,
-                   data=minmax2)+
+  ggplot(aes(x=factor(yearID),y=SO9)) +
+  geom_boxplot(fill='white',color='black') +
   labs(x="Season",
        y="Average Number of Strikeouts per 9 Innings Pitched",
        title="Changes in Strikeouts per 9 Innings Pitched",
        subtitle="Between the 1910 & 2023 Regular Seasons") +
   theme_classic() +
-  scale_y_continuous(limits = c(0,12),
-                     breaks = seq(1,13))
+  scale_x_discrete(breaks=seq(1910,2023,10))
 
-## Well, we need to draw a line to separate the two eras,
-## right?? We can do this with annotate ##
+## Half-Violin Plots ##
+
+## With a half-violin plot, we can see the distribution of 
+## SO9 for each season. We can also include the boxplot to
+## help us visually identify the median and quartiles ##
+
+library(see)
 
 so9 |>
-  ggplot(aes(x=yearID,y=SO9)) +
-  geom_point() +
-  geom_line() +
-  geom_area(aes(y=SO9),
-            fill='blue',
-            alpha = 0.25) +
-  annotate("segment",x=1947,xend=1947,
-           y = 0, 
-           yend = so9 |>
-                  filter(yearID == 1947) |>
-                  select(SO9) |>
-                  as.numeric()
-           ) +
-  geom_label_repel(aes(label=Text),
-                   family = 'serif',
-                   color = 'turquoise',
-                   fontface = 'bold',
-                   nudge_y = 1.5,
-                   size = 3,
-                   data=minmax2)+
+  filter(between(yearID,2015,2022)) |>
+  ggplot(aes(x=factor(yearID),y=SO9)) +
+  geom_violinhalf(fill='green') +
+  geom_boxplot(width = 0.1, fill = 'white') +
   labs(x="Season",
        y="Average Number of Strikeouts per 9 Innings Pitched",
        title="Changes in Strikeouts per 9 Innings Pitched",
-       subtitle="Between the 1910 & 2023 Regular Seasons") +
-  theme_classic() +
-  scale_y_continuous(limits = c(0,12),
-                     breaks = seq(1,13))
-
-## We can play around with the linetype too! ##
-## We have twodash, solid, longdash, dotted, dotdash,
-## and dashed: ##
-
-so9 |>
-  ggplot(aes(x=yearID,y=SO9)) +
-  geom_point() +
-  geom_line() +
-  geom_area(aes(y=SO9),
-            fill='blue',
-            alpha = 0.25) +
-  annotate("segment",x=1947,xend=1947,
-           y = 0, 
-           yend = so9 |>
-             filter(yearID == 1947) |>
-             select(SO9) |>
-             as.numeric(),
-           linetype = 'longdash'
-  ) +
-  geom_label_repel(aes(label=Text),
-                   family = 'serif',
-                   color = 'turquoise',
-                   fontface = 'bold',
-                   nudge_y = 1.5,
-                   size = 3,
-                   data=minmax2)+
-  labs(x="Season",
-       y="Average Number of Strikeouts per 9 Innings Pitched",
-       title="Changes in Strikeouts per 9 Innings Pitched",
-       subtitle="Between the 1910 & 2023 Regular Seasons") +
-  theme_classic() +
-  scale_y_continuous(limits = c(0,12),
-                     breaks = seq(1,13))
-
-## Now, we want to add another annotation mentioning what the 
-## line is partitioning! Let's create another dataframe
-## to do so ##
-
-df2 <- tibble(yearID = c(1930,1987),
-              SO9 = c(1.75,1.75),
-              Text = c("Segregated Era",
-                       "Integrated Era"))
-
-so9 |>
-  ggplot(aes(x=yearID,y=SO9)) +
-  geom_point() +
-  geom_line() +
-  geom_area(aes(y=SO9),
-            fill='blue',
-            alpha = 0.25) +
-  annotate("segment",x=1947,xend=1947,
-           y = 0, 
-           yend = so9 |>
-             filter(yearID == 1947) |>
-             select(SO9) |>
-             as.numeric(),
-           linetype = 'longdash'
-  ) +
-  geom_label_repel(aes(label=Text),
-                   family = 'serif',
-                   color = 'turquoise',
-                   fontface = 'bold',
-                   nudge_y = 1.5,
-                   size = 3,
-                   data=minmax2) +
-  geom_label(aes(label = Text),
-            fontface = 'bold',
-                   data = df2) +
-  labs(x="Season",
-       y="Average Number of Strikeouts per 9 Innings Pitched",
-       title="Changes in Strikeouts per 9 Innings Pitched",
-       subtitle="Between the 1910 & 2023 Regular Seasons") +
-  theme_classic() +
-  scale_y_continuous(limits = c(0,12),
-                     breaks = seq(1,13))
+       subtitle="Between the 2015 & 2022 Regular Seasons") +
+  theme_classic()
